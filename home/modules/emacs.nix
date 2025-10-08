5f36b07938cdb53228a3e6ebe48fbb71aa941f25
oldfart-maker <mkburns61@yahoo.com>
2025-10-08 06:45:01 -0400

    hm(emacs): replace pkgs.emacs-pgtk with versioned fallback

--- home/modules/emacs.nix
+++ home/modules/emacs.nix
@@ -2,7 +2,6 @@
 
 {
   # 1) Install just the Emacs binary (Wayland build on ARM)
-  home.packages = [ pkgs.emacs-pgtk ];
 
   # 2) A shared spot for cross-profile assets; we’ll use it later
   xdg.configFile."emacs-common/.keep".text = "";
@@ -10,7 +9,7 @@
   # 3) Run Emacs as a user systemd daemon; socket-activated
   services.emacs = {
     enable = true;
-    package = pkgs.emacs-pgtk;
+    package = (pkgs.emacs30-pgtk or pkgs.emacs29-pgtk or pkgs.emacs-gtk);
     # Optional, handy: let `editor`/`git commit` use emacsclient
     defaultEditor = true;
     startWithUserSession = true;

bb9992bf89abf6d7d0bd796c712c4c387d1a90be
Mike <mkburns61@yahoo.com>
2025-10-08 17:23:42 -0400

    hm(emacs): clone+tangle emacs_babel_config at switch

--- home/modules/emacs.nix
+++ home/modules/emacs.nix
@@ -1,11 +1,12 @@
 { config, pkgs, lib, ... }:
 let
   emacsPkg = (pkgs.emacs30-pgtk or pkgs.emacs29-pgtk or pkgs.emacs-gtk);
+  repoDir  = "${config.home.homeDirectory}/projects/emacs_babel_config";
 in {
-  # Make emacs/emacsclient available on PATH
-  home.packages = [ emacsPkg ];
+  # Emacs + git on PATH
+  home.packages = [ emacsPkg pkgs.git ];
 
-  # User systemd service: emacs --fg-daemon=emacs-prod
+  # Your existing daemon (keep as-is if you already have it)
   systemd.user.services."emacs-prod" = {
     Unit = {
       Description = "Emacs daemon (emacs-prod)";
@@ -17,9 +18,27 @@ in {
       ExecStart = "${emacsPkg}/bin/emacs --fg-daemon=emacs-prod";
       Restart = "on-failure";
     };
-    Install = { WantedBy = [ "default.target" ]; };
+    Install.WantedBy = [ "default.target" ];
   };
 
-  # nice-to-have
+  # Clone + tangle at HM switch time (runs *every* switch)
+  home.activation.emacsBabelSync = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+    set -eu
+    mkdir -p "$HOME/projects"
+
+    if [ ! -d "${repoDir}/.git" ]; then
+      ${pkgs.git}/bin/git clone https://github.com/oldfart-maker/emacs_babel_config.git "${repoDir}"
+    else
+      ${pkgs.git}/bin/git -C "${repoDir}" fetch --all
+      ${pkgs.git}/bin/git -C "${repoDir}" pull --ff-only
+    fi
+
+    # Tangle in batch (no daemon required)
+    ${emacsPkg}/bin/emacs --batch -l org \
+      --eval '(progn (require (quote ob-tangle))
+                     (org-babel-tangle-file
+                       (expand-file-name "emacs_config.org" "'"${repoDir}"'")))'
+  '';
+
   programs.git.enable = true;
 }

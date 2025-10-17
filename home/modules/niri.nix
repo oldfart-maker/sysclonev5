0c74214b90e7d22de56a4ffecaa9f4fe470d5293
Mike <mkburns61@yahoo.com>
2025-10-17 18:07:58 -0400

    Simple niri.nix install - try 2.

--- home/modules/niri.nix
+++ home/modules/niri.nix
@@ -2,10 +2,8 @@
 let
   niriDir = "${config.home.homeDirectory}/.config/niri";
 in {
-  # Install Niri only. No services.
+  
   home.packages = [ pkgs.niri ];
   
-  home.file.".config/niri/".directory = true;
-
   # '';
 }

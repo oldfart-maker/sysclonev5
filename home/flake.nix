f03345b85dfa0d6e2b1678de18d9ac23000651e2
Mike <mkburns61@yahoo.com>
2025-10-17 04:40:05 -0400

    Pinned HM to v25.11

--- home/flake.nix
+++ home/flake.nix
@@ -1,8 +1,8 @@
 {
   description = "sysclonev5 HM (Pi)";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 

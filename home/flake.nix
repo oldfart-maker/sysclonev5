dc65da0a211ec2bf6b6a95fb19271e5984738c04
Mike <mkburns61@yahoo.com>
2025-10-20 05:50:08 -0400

    Added Stylix.

--- home/flake.nix
+++ home/flake.nix
@@ -4,19 +4,33 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, stylix, ... }:
   let
-    forSystem = system: import nixpkgs { inherit system; config.allowUnfree = true; };
+    # Allow overriding from CLI if you want: --system aarch64-linux
+    system = builtins.currentSystem or "aarch64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
-    homeConfigurations.username = home-manager.lib.homeManagerConfiguration {
-      pkgs = forSystem "aarch64-linux";
+    homeConfigurations."username" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
       modules = [
+        # Import Stylix’ Home Manager module
+        stylix.homeManagerModules.stylix
         ./home.nix
       ];
+      # (Optional) Pass inputs to your modules if you want to reference them
+      extraSpecialArgs = { inherit system; };
     };
   };
 }

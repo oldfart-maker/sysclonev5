7eec20cab69c9328f7c8380cec8acb01a1a93c2e
Mike <mkburns61@yahoo.com>
2025-10-20 08:14:15 -0400

    Fixed fonts.nix error.

--- home/modules/fonts.nix
+++ home/modules/fonts.nix
@@ -1,9 +1,14 @@
 { config, pkgs, lib, ... }:
 {
   fonts.fontconfig.enable = true;
+
   home.packages = [
     pkgs.jetbrains-mono
     pkgs.fira-code
     pkgs.cantarell-fonts
+
+    # new way: separate nerd-fonts namespace
+    pkgs.nerd-fonts.symbols-only   # provides all Waybar icon glyphs
+    pkgs.noto-fonts-emoji
   ];
 }

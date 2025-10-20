4079fb4101deeafbe102c2e84b2bb8a652879f37
Mike <mkburns61@yahoo.com>
2025-10-19 20:17:06 -0400

    Added alacritty overlay for wayland support.

--- home/flake.nix
+++ home/flake.nix
@@ -10,13 +10,26 @@
 
   outputs = { self, nixpkgs, home-manager, ... }:
   let
-    forSystem = system: import nixpkgs { inherit system; config.allowUnfree = true; };
+    # NEW: overlay which forces Alacritty to build with Wayland + X11
+    overlays = [
+      (final: prev: {
+        alacritty = prev.alacritty.override {
+          waylandSupport = true;
+          x11Support = true;
+        };
+      })
+    ];
+
+    forSystem = system:
+      import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = overlays;        # NEW
+      };
   in {
     homeConfigurations.username = home-manager.lib.homeManagerConfiguration {
       pkgs = forSystem "aarch64-linux";
-      modules = [
-        ./home.nix
-      ];
+      modules = [ ./home.nix ];
     };
   };
 }

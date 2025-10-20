2833b981de98c1e3cf32451a906e1a19f4cc1ee8
Mike <mkburns61@yahoo.com>
2025-10-20 06:23:56 -0400

    Fixed syntax in stylix.nix.

--- home/modules/stylix.nix
+++ home/modules/stylix.nix
@@ -1,24 +1,23 @@
 # modules/stylix.nix
 { config, pkgs, lib, ... }:
-
 {
-  # Pick ONE: image-based palette OR a fixed Base16 scheme.
   stylix = {
     enable   = true;
 
-    # Option A: derive palette from an image
-    image    = "${config.home.homeDirectory}/Pictures/wallpapers/waves.jpg";
-    polarity = "dark";
-
-    # Option B: fixed Base16 scheme (uncomment & remove `image` above)
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    # Pick ONE of these sources (image OR base16 scheme)
+    # image    = "${config.home.homeDirectory}/Pictures/wallpapers/waves.jpg";
+    # polarity = "dark";
+    
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
     targets = {
       waybar = {
         enable = true;
-        addCss = false;  # we’ll provide our own style/colors
+        addCss = false;  # we'll provide our own style/colors
       };
-      rofi.enable = true;
+      rofi = {
+        enable = true;
+      };
     };
-  }
+  };
 }

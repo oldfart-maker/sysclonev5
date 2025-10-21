d6b2ce1e4b8da5368455097dd6b8e2158e09a9e4
Mike <mkburns61@yahoo.com>
2025-10-20 20:06:06 -0400

    Fixed bug.

--- home/modules/system-theme.nix
+++ home/modules/system-theme.nix
@@ -1,26 +1,30 @@
-stylix = {
-  enable = true;
-  # image = "/home/username/Pictures/walls/gruv-rock.jpg";
-  base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-  polarity = "dark";
+# modules/system-theme.nix
+{ config, pkgs, lib, ... }:
+{
+  stylix = {
+    enable = true;
+  # image = "/home/username/Pictures/walls/gruv-rock.jpg";    
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    polarity = "dark";
 
-  fonts = {
-    monospace = { package = pkgs.jetbrains-mono; name = "JetBrains Mono"; };
-    sansSerif = { package = pkgs.dejavu_fonts;  name = "DejaVu Sans";    };
-    serif     = { package = pkgs.dejavu_fonts;  name = "DejaVu Serif";   };
-    emoji     = { package = pkgs.noto-fonts-emoji; name = "Noto Color Emoji"; };
-  };
+    fonts = {
+      monospace = { package = pkgs.jetbrains-mono; name = "JetBrains Mono"; };
+      sansSerif = { package = pkgs.dejavu_fonts;  name = "DejaVu Sans";    };
+      serif     = { package = pkgs.dejavu_fonts;  name = "DejaVu Serif";   };
+      emoji     = { package = pkgs.noto-fonts-emoji; name = "Noto Color Emoji"; };
+    };
 
-  cursor = {
-    package = pkgs.qogir-icon-theme;
-    name    = "Qogirr";
-    size    = 12;
-  };
+    cursor = {
+      package = pkgs.qogir-icon-theme;
+      name    = "Qogirr";
+      size    = 12;
+    };
 
-  targets = {
-    waybar = { enable = false; addCss = false; };
-    rofi.enable = false;
-    foot.enable = false;
-    # alacritty.enable = true; # example if you want Stylix to manage it
+    targets = {
+      waybar = { enable = false; addCss = false; };
+      rofi.enable = false;
+      foot.enable = false;
+      # alacritty.enable = true;
+    };
   };
-};
+}

1d0d305478a819a9582aed8f4afe6b8874e88dbe
Mike <mkburns61@yahoo.com>
2025-10-23 15:50:29 -0400

    Refactored.

--- home/modules/niri.nix
+++ home/modules/niri.nix
@@ -2,20 +2,21 @@
 { config, lib, ... }:
 
 let
-  # Use path literals (pure). These are relative to this file's location.
   cfgPath  = ../../generated/niri/config.kdl;
   keysPath = ../../generated/niri/key_bindings.txt;
-
   niriDir  = "${config.xdg.configHome}/niri";
 in
 {
-  # 1) Always install config.kdl from the repo
-  home.file."${niriDir}/config.kdl".text = builtins.readFile cfgPath;
+  # Always install config.kdl from the repo
+  home.file =
 
-  # 2) Optionally install key_bindings.txt if present in the repo
-  #    (pure check during evaluation)
-  #    If the file isn't there, nothing is created.
-  lib.mkIf (builtins.pathExists keysPath) {
-    home.file."${niriDir}/key_bindings.txt".text = builtins.readFile keysPath;
-  }
+    # Conditionally add key_bindings.txt if it exists (pure, eval-time)
+    (lib.optionalAttrs (builtins.pathExists keysPath) {
+      "${niriDir}/key_bindings.txt".text = builtins.readFile keysPath;
+    })
+
+    # Always-present entries go here
+    // {
+      "${niriDir}/config.kdl".text = builtins.readFile cfgPath;
+    };
 }

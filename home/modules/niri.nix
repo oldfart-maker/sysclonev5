7a0cc69b3301754cc1d65dc85a5c1bd3552eb639
Mike <mkburns61@yahoo.com>
2025-10-23 18:16:38 -0400

    Change path issue.

--- home/modules/niri.nix
+++ home/modules/niri.nix
@@ -7,13 +7,10 @@ let
   niriDir  = "${config.xdg.configHome}/niri";
 in
 {
-  # Build a single home.file attrset:
   home.file =
-    # Conditionally add key_bindings.txt if it exists (pure, eval-time)
     (lib.optionalAttrs (builtins.pathExists keysPath) {
       "${niriDir}/key_bindings.txt".text = builtins.readFile keysPath;
     })
-    # Always add config.kdl, and force our definition to win over any other module.
     // {
       "${niriDir}/config.kdl" = lib.mkForce {
         text = builtins.readFile cfgPath;
